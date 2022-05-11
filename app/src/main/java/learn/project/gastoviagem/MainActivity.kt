6a4aa1be47d5e70d22431ce7fc64946c6b189917
João Eduardo dos Santos Almeida <joao.santos@ifs.edu.br>
2022-05-11 16:23:15 -0300

    project Update

--- app/src/main/java/learn/project/gastoviagem/MainActivity.kt
+++ app/src/main/java/learn/project/gastoviagem/MainActivity.kt
@@ -1,11 +1,17 @@
 package learn.project.gastoviagem
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import learn.project.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding.editDistance
+
     }
 }
